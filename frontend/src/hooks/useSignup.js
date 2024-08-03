6f2016127f4c20bf392fa6ed9d50ae6510e1f75f
Anirudhchauhan04 <chauhan.anirudh004@gmail.com>
2024-08-04 02:48:39 +0530

    completed

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
-
-const useSignup =() => {
-    const [loading, setLoading] = useState(false);
+const useSignup = () => {
+	const [loading, setLoading] = useState(false);
 	const { setAuthUser } = useAuthContext();
 
 	const signup = async ({ fullName, username, password, confirmPassword, gender }) => {
@@ -32,9 +33,7 @@ const useSignup =() => {
 
 	return { loading, signup };
 };
-
 export default useSignup;
-
 
 function handleInputErrors({ fullName, username, password, confirmPassword, gender }) {
 	if (!fullName || !username || !password || !confirmPassword || !gender) {
